64f1f9c7eca3f27e4d224e060ea177f1b5fd42c9
Chris Haas <chris@vendiadvertising.com>
2019-07-15 16:38:39 -0500

    Cleanup, bail early, anonymous functions

--- acf-gravity_forms.php
+++ acf-gravity_forms.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Advanced Custom Fields: Gravity Forms Field
-Plugin URI: https://github.com/stormuk/Gravity-Forms-ACF-Field
+Plugin URI: https://github.com/vendi-advertising/gravity-forms-acf-field
 Description: ACF field to select one or many Gravity Forms
 Version: 1.1.0
 Author: @adam_pope of @stormuk
@@ -10,39 +10,36 @@ License: MIT
 License URI: http://opensource.org/licenses/MIT
 */
 
-// $version = 5 and can be ignored until ACF6 exists
-function include_field_types_Gravity_Forms( $version ) {
-
-  require_once __DIR__ . '/src/v5/gravity_forms-v5.php';
-  new acf_field_gravity_forms();
+//No WordPress. Huh, weird.
+if(!defined('ABSPATH')){
+	return;
 }
 
-add_action('acf/include_field_types', 'include_field_types_gravity_forms');
-
-
-function register_fields_Gravity_Forms() {
-  require_once __DIR__ . '/src/v4/gravity_forms-v4.php';
-  new acf_field_gravity_forms();
+//No ACF
+if(!class_exists('acf_field')){
+	return;
 }
 
-add_action('acf/register_fields', 'register_fields_gravity_forms');
-
-//Added to check if Gravity Forms is installed on activation.
-function gff_activate() {
-
-    if (class_exists('RGFormsModel')) {
-
-			return true;
-
-		}	else {
-
-			$html = '<div class="error">';
-				$html .= '<p>';
-					$html .= _e( 'Warning: Gravity Forms is not installed or activated. This plugin does not function without Gravity Forms!' );
-				$html .= '</p>';
-			$html .= '</div>';
-			echo $html;
-
-		}
+//No Gravity forms
+if(!class_exists('RGFormsModel')){
+	return;
 }
-register_activation_hook( __FILE__, 'gff_activate' );
+
+add_action(
+	'acf/include_field_types',
+	function(){
+		require_once __DIR__ . '/src/gravity_forms_base.php';
+		require_once __DIR__ . '/src/v5/gravity_forms-v5.php';
+		new acf_field_gravity_forms();
+	}
+);
+
+//Really, this should just be killed off now
+add_action(
+	'acf/register_fields',
+	function() {
+		require_once __DIR__ . '/src/gravity_forms_base.php';
+  		require_once __DIR__ . '/src/v4/gravity_forms-v4.php';
+  		new acf_field_gravity_forms();
+	}
+);
